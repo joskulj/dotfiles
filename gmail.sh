2f6c83c66a0e5c275d547675914ff636463cfa53
Jochen Skulj <jochen@jochenskulj.de>
2012-10-06 22:21:02 +0200

    Mail configuration updated.

---
+++ gmail.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp /home/joskulj/.msmtprc-gmail /home/joskulj/.msmtprc
+cp /home/joskulj/.mutt/gmail /home/joskulj/.mutt/account
+mutt
---
+++ smail.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp /home/joskulj/.msmtprc-strato /home/joskulj/.msmtprc
+cp /home/joskulj/.mutt/maildir /home/joskulj/.mutt/account
+mutt
